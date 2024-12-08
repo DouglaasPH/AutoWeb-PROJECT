03e1e1b8c7802f0e1fd4ec4353d6829785d6770c
Douglas Phelipe <douglasphelipedpss@gmail.com>
2024-12-08 00:55:50 -0300

    feature: Adicionado novo método para manipulação de dados do usuário. Este arquivo visa limpar a repetição de código eem cada componente que for preciso os dados de usuário.

---
+++ frontend/src/dados da conta/dados_da_conta.ts
@@ -0,0 +1,35 @@
+interface TIPO_DE_DADOS {
+    [key: string]: string,
+}
+
+const PEGAR_DADOS: null | string = localStorage.getItem("dados_do_usuário");
+let DADOS_DO_USUARIO: string | TIPO_DE_DADOS = "não logado" ;
+let LOGGINED: boolean = false;
+
+function VERIFICAR_ESTADO_DE_LOGIN() {
+    if (PEGAR_DADOS == null) {
+        DADOS_DO_USUARIO = "não logado";
+        LOGGINED = DADOS_DO_USUARIO !== "não logado";
+    } else {
+        try {
+            const parsedDados = JSON.parse(PEGAR_DADOS);
+            if (typeof parsedDados === "object" && parsedDados !== null) {
+                DADOS_DO_USUARIO = parsedDados;
+                LOGGINED = DADOS_DO_USUARIO !== "não logado";
+            } else {
+                DADOS_DO_USUARIO = "não logado";
+                LOGGINED = DADOS_DO_USUARIO !== "não logado";
+            }
+            LOGGINED = DADOS_DO_USUARIO !== "não logado";
+        } catch (error) {
+            LOGGINED = DADOS_DO_USUARIO !== "não logado";
+            DADOS_DO_USUARIO = "não logado";
+            console.error("erro ao parsear dados do usuário:", error);
+        }
+    };
+};
+
+VERIFICAR_ESTADO_DE_LOGIN();
+
+export default DADOS_DO_USUARIO;
+export { VERIFICAR_ESTADO_DE_LOGIN, LOGGINED };
